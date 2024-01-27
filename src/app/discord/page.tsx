08d986834cd086f566b1d2ca99772f3c489102b2
Luis Carlin <luiscarlin@gmail.com>
2024-01-27 10:30:49 -0500

    better styling

--- src/app/discord/page.tsx
+++ src/app/discord/page.tsx
@@ -30,9 +30,12 @@ export default function Discord() {
 const SidebarIcon = ({ children }: { children: React.ReactNode }) => {
   return (
     <i
-      className="relative flex items-center justify-center h-12 w-12 my-2 mx-auto shadow-lg 
-                  bg-gray-800 text-green-500 hover:bg-green-600 hover:text-white 
-                  rounded-full hover:shadow-xl cursor-pointer transition-all"
+      className="relative flex items-center justify-center h-12 w-12 my-2 mx-auto
+                bg-gray-400 hover:bg-green-600 dark:bg-gray-800
+                text-green-500 hover:text-white
+                  rounded-3xl hover:rounded-xl
+                  transition-all duration-300 ease-linear
+                  cursor-pointer shadow-lg"
     >
       {children}
     </i>
